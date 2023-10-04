e9303c6f24626ac6eddf5a4e0e9d1d24585f957f
Mohamed <mohameddrame907@gmail.com>
2023-10-04 21:57:42 +0200

    refacto/ put search struct in searchViewModel

--- weathr/Controller/SearchTableViewController.swift
+++ weathr/Controller/SearchTableViewController.swift
@@ -7,42 +7,6 @@
 
 import UIKit
 
-struct Weather: Codable {
-    let location: Location
-    let current: Current
-    let forecast : Forecast
-}
-
-struct Location: Codable {
-    let name: String
-    let region: String
-}
-
-struct Current: Codable {
-    let temp_c: Double
-    let condition: Condition
-}
-
-struct Forecast : Codable  {
-    let forecastday: [ForecastDay]
-}
-
-struct ForecastDay : Codable  {
-    let date: String
-    let day: Day
-}
-
-struct Day : Codable  {
-    let maxtemp_c: Double
-    let mintemp_c: Double
-    let condition: Condition
-}
-
-struct Condition : Codable  {
-    let text: String
-    let icon: String
-}
-
 class WeatherCityCell: UITableViewCell {
     @IBOutlet weak var cityWeatherLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
